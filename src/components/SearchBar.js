1e60ee396d16d25e013468210f62283e1983ea24
Fatih Aydemir <fatihaydemir23@gmail.com>
2023-02-09 10:31:05 +0300

    icon packages added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 
 const SearchBar = () => {
   return (
     <View style={styles.background}>
+      <Icon name='done'/>
       <Text>Search Bar</Text>
     </View>
   );
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import SearchBar from '../components/SearchBar'
+
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   return (
