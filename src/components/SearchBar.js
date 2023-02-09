3de1049a7f3461eeabe8b8926e6ef5a1e3306cbf
Fatih Aydemir <fatihaydemir23@gmail.com>
2023-02-09 11:07:18 +0300

    search bar stylings

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, TextInput, StyleSheet} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Icon name='done'/>
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Icon name='search' style={styles.iconStyle}/>
+      <TextInput 
+      style={styles.inputStyle}
+      placeholder='Search'
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    background: {
-        backgroundColor: 'grey',
+    backgroundStyle: {
+        marginTop: 10,
+        backgroundColor: '#B2B2B2',
         height: 50,
         borderRadius: 5,
-        marginHorizontal: 15
+        marginHorizontal: 15,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+      flex: 1,
+      fontSize: 18
+    },
+    iconStyle: {
+      fontSize: 35,
+      alignSelf: 'center',
+      marginHorizontal: 5
     }
 });
 
 export default SearchBar;
+ 
